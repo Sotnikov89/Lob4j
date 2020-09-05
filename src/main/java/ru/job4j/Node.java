7081e0d6aaa4c0a795bc41ab2b45a050bb324f08
Vadim Sotnikov <sotnikovvu@gmail.com>
2020-09-05 13:40:03 +0300

    3. Immutable объекты [#354428]

---
+++ src/main/java/ru/job4j/Node.java
@@ -0,0 +1,21 @@
+package ru.job4j;
+import net.jcip.annotations.NotThreadSafe;
+
+@NotThreadSafe
+public class Node<T> {
+    private final Node next;
+    private final T value;
+
+    public Node(Node next, T value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
