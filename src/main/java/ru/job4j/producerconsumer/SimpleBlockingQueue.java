f0d74fe47e52f7e5c42bb8241c199a4c3022c86b
Vadim Sotnikov <sotnikovvu@gmail.com>
2020-09-10 15:44:51 +0300

    Реализовать шаблон Producer Consumer. [#354416]

--- src/main/java/ru/job4j/producerconsumer/SimpleBlockingQueue.java
+++ src/main/java/ru/job4j/producerconsumer/SimpleBlockingQueue.java
@@ -11,36 +11,25 @@ public class SimpleBlockingQueue<T> {
 
     @GuardedBy("this")
     private final Queue<T> queue = new LinkedList<>();
+    private final int maxQueueSize;
+
+    public SimpleBlockingQueue(int maxQueueSize) {
+        this.maxQueueSize = maxQueueSize;
+    }
 
     public synchronized void offer(T value) throws InterruptedException {
-        Thread.sleep(1000); // With this pause you have 100% guaranteed to see a consumer's massage about empty queue.
-        System.out.println(Thread.currentThread().getName()
-                + " - Добавляю в очередь объект - " + value + ".");
-        if (queue.offer(value)) {
-            System.out.println(Thread.currentThread().getName()
-                    + " - Объект добавлен. Возвращаю всех потребителей в работу.");
-            this.notifyAll();
-        } else {
-            throw new RuntimeException("Не удалось добавить объект в очередь");
+        if (queue.size() == maxQueueSize) {
+            this.wait();
         }
+        this.notifyAll();
+        queue.offer(value);
     }
 
     public synchronized T poll() throws InterruptedException {
-        while (queue.size() == 0){
-            System.out.println(Thread.currentThread().getName()
-                    + " - В очереди нет объектов. Перехожу в состояние "
-                    + Thread.State.WAITING + ".");
+        if (queue.isEmpty()) {
             this.wait();
         }
-        System.out.println(Thread.currentThread().getName()
-                + " - Беру объект из очереди.");
-        T t = queue.poll();
-        System.out.println(Thread.currentThread().getName()
-                + " - Объект - " + t + ", получен.");
-        return t;
-    }
-
-    public synchronized Queue<T> getQueue() {
-        return queue;
+        this.notifyAll();
+        return queue.poll();
     }
 }
--- src/main/java/ru/job4j/producerconsumer/SimpleBlockingQueueTest.java
+++ src/main/java/ru/job4j/producerconsumer/SimpleBlockingQueueTest.java
@@ -2,10 +2,8 @@ package ru.job4j.producerconsumer;
 
 public class SimpleBlockingQueueTest {
 
-    public static void main(String[] args) throws InterruptedException{
-        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
-
-        System.out.println("Программа запущена");
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(2);
 
         Thread producer1 = new Thread(() -> {
             try {
@@ -13,16 +11,18 @@ public class SimpleBlockingQueueTest {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }, "Producer1");
+        });
         Thread consumer1 = new Thread(() -> {
             try {
                 queue.poll();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }, "Сonsumer1");
+        });
 
-        consumer1.start();
         producer1.start();
+        producer1.join();
+        consumer1.start();
+        consumer1.join();
     }
 }
