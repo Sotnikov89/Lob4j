5838d22059a49b3f205c23333904bce9a6fb55e1
Vadim Sotnikov <sotnikovvu@gmail.com>
2020-09-16 13:43:01 +0300

    2. ExecutorService рассылка почты. [#354409]

--- src/main/java/ru/job4j/EmailNotification.java
+++ src/main/java/ru/job4j/EmailNotification.java
@@ -11,9 +11,9 @@ public class EmailNotification {
         pool.submit(new Runnable() {
             @Override
             public void run() {
-                String suject = "Notification " + user.userName + " to email " + user.email;
+                String subject = "Notification " + user.userName + " to email " + user.email;
                 String body = "Add a new event to " + user.userName;
-                send(suject, body, user.email);
+                send(subject, body, user.email);
             }
         });
     }
@@ -22,36 +22,12 @@ public class EmailNotification {
         pool.shutdown();
     }
 
-    public void send(String suject, String body, String email) {
+    public void send(String subject, String body, String email) {
 
     }
 
     public static class User {
         private String userName;
         private String email;
-
-        public User() {
-        }
-
-        public User(String userName, String email) {
-            this.userName = userName;
-            this.email = email;
-        }
-
-        public String getUserName() {
-            return userName;
-        }
-
-        public void setUserName(String userName) {
-            this.userName = userName;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-        }
     }
 }
