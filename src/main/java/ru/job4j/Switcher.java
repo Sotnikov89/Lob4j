42add1216ce8f83c7847be4f203ca5dee17f11e4
Vadim Sotnikov <sotnikovvu@gmail.com>
2020-09-24 13:13:14 +0300

    2. Thread switcher [#354445]

---
+++ src/main/java/ru/job4j/Switcher.java
@@ -0,0 +1,54 @@
+package ru.job4j;
+
+public class Switcher {
+
+    public synchronized void on() {
+            this.notify();
+    }
+
+    public synchronized void check() {
+        try {
+            this.wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Switcher switcher = new Switcher();
+
+        Thread first = new Thread(
+                () -> {
+                    while (true) {
+                        System.out.println("Thread A");
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        switcher.on();
+                        switcher.check();
+                    }
+                }
+        );
+        Thread second = new Thread(
+                () -> {
+                    while (true) {
+                        switcher.check();
+                        System.out.println("Thread B");
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        switcher.on();
+                    }
+                }
+        );
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+    }
+}
