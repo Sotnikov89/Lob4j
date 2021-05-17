22e3a71ad82da5ad593a7693421b3e0073be1a3b
Sotniko89git config --global user.name Sotniko89git config --global user.name Sotniko89 <sotnikovvu@gmail.com>
2021-05-17 09:41:14 +0300

    del parser

--- src/main/java/ru/job4j/parserfile/GetContent.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.parserfile;
-
-import javax.sql.rowset.Predicate;
-import java.io.File;
-
-public interface GetContent {
-    public String getContent(File file, Predicate filter);
-}
